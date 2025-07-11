c142d4a49c8ea8cfc1117c92c7c4957d60c850db
Mehmet Sami Boz <69392678+msamiboz@users.noreply.github.com>
2025-07-11 16:18:51 +0300

    add dictionary to report

--- R/Dictionary_generator.R
+++ R/Dictionary_generator.R
@@ -1,7 +1,7 @@
 terms <- list(
   PNS = "Term found in dataset guessed as it stands for planning",
   "Early Cost Overrun Warning" = "this warning determined by comparing estimate spending is higher than budget. Phase level.",
-  "Realized Cost Overrun Warning": "this warning determined by comparing actual spending is higher than the budget. Phase level.",
+  "Realized Cost Overrun Warning"= "this warning determined by comparing actual spending is higher than the budget. Phase level.",
   "Realized Delay Warning"=  "Actual end date is later than planned end date. Phase level",
   "Early Delay Warning"= "For In-progress phases that planned end date is later than dataset time which is last end date observed: end of March 2025. Phase level",
   "Critical Cost Overrun and Delay Warning"= "Raised if both Cost and delay warnings are active.Phase level.",
@@ -18,7 +18,15 @@ terms <- list(
   "Project Budget Amount" = "Award base budget by phase",
   "Final Estimate of Actual Costs Through End of Phase Amount" = "Current projected final estimate at completion of project by phase",
   "Total Phase Actual Spending Amount" = "Actual cumulative expenditures by phase",
-  "DSF Number(s)" = "Number used to identify project in Five Year Plan"
+  "DSF Number(s)" = "Number used to identify project in Five Year Plan",
+  Phase= "Each seperate part of a project",
+  KPI= "Key performance indicator",
+  EDA= "Explotary data analysis",
+  "CM,F&E" = "Construction Management, Furniture & Equipment – (Guessed)",
+  "Construction" = "Main construction phase – (Guessed)",
+  "Design" = "Architectural and engineering design – (Guessed)",
+  "Purch & Install" = "Purchase and Installation – (Guessed)",
+  "Scope" = "Scoping and pre-design – (Guessed)"
 )
 
 for (term in names(terms)){
